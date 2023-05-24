cc351184e112fcb7f2ef77bc0f7158927750c31c
Hriday Keswani <hridaykeswani@gmail.com>
2023-05-24 17:04:39 +0530

    changes in controller

--- lib/controllers/mentee_controller.dart
+++ lib/controllers/mentee_controller.dart
@@ -1,7 +1,8 @@
 import 'package:kwtd/models/mentee.dart';
 import 'package:http/http.dart' as http;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 Future<Mentee> getMenteeDetails() async {
-  var res = await http.get('http://192.168.29.6:8080/mentor' as Uri);
+  var res = await http.get('<api_addr>/mentor' as Uri);
   return menteeFromJson(res.body);
 }
