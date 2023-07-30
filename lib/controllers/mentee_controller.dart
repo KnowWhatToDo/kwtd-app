ffe123941e8a74585d704c5c93ff6fe346019443
Hriday Keswani <hridaykeswani@gmail.com>
2023-07-30 09:31:35 +0530

    implement mentee controller documentation

--- lib/controllers/mentee_controller.dart
+++ lib/controllers/mentee_controller.dart
@@ -1,7 +1,11 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:kwtd/models/mentee.dart';
 import 'package:http/http.dart' as http;
+import 'package:flutter/foundation.dart';
 
+/// Gets the details of a mentee by the individiual's number
+/// [param] Takes [String] number as parameter
+/// [returns] a [Future] of [Mentee] object with details of the Mentee
 Future<Mentee> getMentee(String number) async {
   await dotenv.load(fileName: 'assets/.env');
   late http.Response res;
@@ -10,11 +14,16 @@ Future<Mentee> getMentee(String number) async {
       Uri.parse('${dotenv.get('TEST_ADDRESS')}/getMentee?phone=$number'),
     );
   } catch (error) {
-    print(error);
+    if (kDebugMode) {
+      print(error);
+    }
   }
   return menteeFromJson(res.body);
 }
 
+/// Creates a [Mentee] in the database
+/// [param] Take [Mentee] object as a parameter
+/// [returns] nothing
 Future<void> createMentee(Mentee mentee) async {
   await dotenv.load(fileName: 'assets/.env');
   await http.post(
@@ -26,6 +35,9 @@ Future<void> createMentee(Mentee mentee) async {
   );
 }
 
+/// Updates an existing [Mentee] in the database
+/// [param] Take [Mentee] object as a parameter
+/// [returns] nothing
 Future<void> updateMentee(Mentee mentee) async {
   await http.post(
     Uri.parse('${dotenv.get('TEST_ADDRESS')}/addMentee'),
